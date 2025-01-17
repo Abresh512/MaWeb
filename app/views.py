629888942ee667e9ddeec7d4f01464655ed71588
Abresh512 <abrahamtadesse5123@gmail.com>
2025-01-17 15:15:56 +0300

    new

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from .forms import RegisterForm
 from .models import Registration
@@ -9,28 +11,28 @@ def index(request):
 
 def register(request):
     form = RegisterForm(request.POST or None)
-    if request.user.is_authenticated:
-        if request.method == 'POST':
-            if form.is_valid():
-                register = form.save()
-                return redirect('output')
-        else:
-            return render(request, 'pages/register.html', {'form':form})
-    
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('output')
+        
+    return render(request, 'pages/register.html', {'form':form})
 
 def login(request):
     return render(request, 'pages/login.html')
 
 
 def output(request):
-    records = Registration.objects.all()
-    if request.method == 'post':
-        username = request.POST.get['username']
-        password = request.POST.get['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('output')
-        else:
-            return redirect('index')
-    return render(request, 'pages/output.html', {'records':records})
+    if request.user.is_authenticated:
+        records = Registration.objects.all()
+        if request.method == 'post':
+            username = request.POST.get['username']
+            password = request.POST.get['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('output')
+            else:
+                return redirect('index')
+        return render(request, 'pages/output.html', {'records':records})
+    return redirect('index')
